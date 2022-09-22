3b09824067e39272df5c7b70c93095206a0bf0c9
bloweyelashwish <yelizaveta.barkar@gmail.com>
2022-09-22 14:14:48 +0300

    useField hook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Routes, Route, Link, useParams, useMatch, useNavigate
 } from "react-router-dom"
+import { useField } from "./hooks";
 
 const Menu = () => {
   const padding = {
@@ -63,21 +64,22 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+    const info = useField('text')
+    console.log(content, author, info)
     const navigate = useNavigate()
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-      props.createNotification(`${content} by ${author} was added to the list`)
+      props.createNotification(`${content.value} by ${author.value} was added to the list`)
       navigate('/')
   }
 
@@ -87,15 +89,15 @@ const CreateNew = (props) => {
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <input {...content} />
           </div>
           <div>
             author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+            <input {...author} />
           </div>
           <div>
             url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <input {...info} />
           </div>
           <button>create</button>
         </form>
